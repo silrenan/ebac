2cd64e1cc028a1722b30e4cb334e12741bcdfe65
Renan <vanguarda101@gmail.com>
2023-07-23 22:35:58 -0400

    update 001

---
+++ module-16/test/specs/task.spec.js
@@ -0,0 +1,7 @@
+describe('module 16 task', function() {
+    it('should complete module 16 task', async function() {
+        setTimeout(()=>{
+            console.log('abriu')
+        }, 10000)
+    });
+})
